b9a044a461386eabc43b30d01c6081645a77b8b9
Ricardo Cabrera <114771338+Ricardo0609@users.noreply.github.com>
2025-10-01 13:44:24 -0600

    Delete public directory

--- public/database.js
+++
@@ -1,9 +0,0 @@
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
-
-export async function openDB() {
-  return open({
-    filename: "./tunepath-backend/tunepath.db", // ruta de tu base de datos
-    driver: sqlite3.Database
-  });
-}
